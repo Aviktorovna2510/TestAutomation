0bdaf528cdc579374869d7edd1fada0c07620d84
Anastasia Fedirchyk <anastasiya.fe@onyxenterprises.com>
2018-08-09 23:20:49 +0300

    Added global constants class

---
+++ src/main/java/constants/GlobalConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class GlobalConstants {
+
+    // Chrome Drivers paths
+    public static final String CHROME_DRIVER_WINDOWS_PATH = System.getProperty("user.dir") + "/src/main/resources/chromedriver";
+    public static final String CHROME_DRIVER_MAC_OS_PATH = System.getProperty("user.dir") + "/src/main/resources/chromedriver";
+
+    public static final String START_PAGE = "https://sravnizajm.com.ua/vyberi-luchshij-kredit-onlajn/";
+}
