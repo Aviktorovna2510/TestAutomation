c7c8a4771ec5e6fda03cdb00f70fffa23ebc03c6
Anastasia Fedirchyk <anastasiya.fe@onyxenterprises.com>
2018-08-09 23:51:55 +0300

    Added ChromeDriver.exe

--- src/main/java/constants/GlobalConstants.java
+++ src/main/java/constants/GlobalConstants.java
@@ -3,8 +3,8 @@ package constants;
 public class GlobalConstants {
 
     // Chrome Drivers paths
-    public static final String CHROME_DRIVER_WINDOWS_PATH = System.getProperty("user.dir") + "/src/main/resources/chromedriver";
-    public static final String CHROME_DRIVER_MAC_OS_PATH = System.getProperty("user.dir") + "/src/main/resources/chromedriver";
+    public static final String CHROME_DRIVER_WINDOWS_PATH = System.getProperty("user.dir") + "/src/main/resources/windows/chromedriver.exe";
+    public static final String CHROME_DRIVER_MAC_OS_PATH = System.getProperty("user.dir") + "/src/main/resources/macos/chromedriver";
 
     public static final String START_PAGE = "https://sravnizajm.com.ua/vyberi-luchshij-kredit-onlajn/";
 }
