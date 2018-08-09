e40def3fff5a05b741a0a0e5769cc2171a56527a
Anastasia Fedirchyk <anastasiya.fe@onyxenterprises.com>
2018-08-09 23:22:53 +0300

    Changed MyWallet logic

--- src/main/java/logic/LogicMyWallet.java
+++ src/main/java/logic/LogicMyWallet.java
@@ -1,44 +1,48 @@
 package logic;
 
 import elements.ElementsMyWallet;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 
-public class LogicMyWallet extends MainLogic{
+public class LogicMyWallet extends MainLogic {
+
     private ElementsMyWallet elements;
+    private WebDriver driver;
 
     public LogicMyWallet(WebDriver driver, WebDriverWait wait, ElementsMyWallet elements) {
         super(driver, wait);
         this.elements = elements;
+        this.driver = driver;
     }
 
-    public void useJavaScriptForSliderStyleChanging() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("document.querySelector('#amountval .noUi-handle-lower').setAttribute('class','noUi-handle noUi-handle-lower noUi-active');");
-        js.executeScript("document.querySelector('#amountval .noUi-origin').style=\"left: 100%;\";");
-        js.executeScript("document.querySelector('#amountval .noUi-connect').style=\"left: 0%; right: 0%;\";");
-        js.executeScript("document.querySelector('#amountval .noUi-handle-lower').setAttribute('aria-valuemax','100.0');");
-        js.executeScript("document.querySelector('#amountval .noUi-handle-lower').setAttribute('aria-valuenow','100.0');");
-        js.executeScript("document.querySelector('#amountval .noUi-handle-lower').setAttribute('aria-valuetext','6000');");
-        //js.executeScript("document.querySelector('#amountval-val').setAttribute('style','6000');");
-        waitForJSToBeLoaded();
-        waitForJQueryToBeLoaded();
-    }
-
-    public void dragAndDrop () {
+    /**
+     * Step 4 - Verify that credit is available for sum 6000
+     */
+    public void verifyCreditAvailability() {
         waitForVisible(elements.slider);
-        WebElement slider = driver.findElement(By.cssSelector("#amountval .noUi-handle-lower"));
         Actions move = new Actions(driver);
-        Action action = (Action) move.dragAndDropBy(slider, 100, 0).build();
+        Action action = move.dragAndDropBy(elements.slider, 100, 0).build();
         action.perform();
     }
 
+    /**
+     * Step 5 - Resolution verifying
+     */
+    public void verifyResult() {
+        final int limit = 6000;
+        final String emptyString = "",
+                whitespace = " ";
 
+        waitForVisible(elements.amountalValue);
+        String maximumValue = elements.amountalValue.getText().replace(whitespace, emptyString);
 
-
+        if (limit <= Integer.parseInt(maximumValue)) {
+            Reporter.log("КРЕДИТ ВЗЯТЬ МОЖНО");
+        } else {
+            Reporter.log("КРЕДИТ ВЗЯТЬ НЕЛЬЗЯ");
+        }
+    }
 }
